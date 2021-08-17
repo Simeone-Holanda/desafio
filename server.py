87f9d20e0f921fddf064e8ccfe7eaad77e706fac
Simeone-Holanda <simeone.10s10@gmail.com>
2021-08-17 01:25:57 -0300

    [CREATED]: We start the database connection and apply the CLI to the execution file

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
+from src.infrastructure.config import Database
+from src.applications.CLI import import_csv
 from src.app import app
 
+try:
+    Database.set_connection()
+    #import_csv()
+    app.run(port=8000)
+except Exception as ex:
+    print("Error - ",ex)
+    raise ex
 
-app.run(port=8000)
