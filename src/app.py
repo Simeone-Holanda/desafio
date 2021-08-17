23c7cc0b597709a62d681a9ddffe7f97503deb98
Simeone-Holanda <simeone.10s10@gmail.com>
2021-08-17 01:32:55 -0300

    [Added]: Endpoint module to flask

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from src.api.card.card_api import CardAPI
+from src.api.tag.tag_api import TagAPI
 
 app = Flask(__name__)
 
 app.secret_key = "my secret key"
 
+app.register_blueprint(CardAPI)
+app.register_blueprint(TagAPI)
 
-@app.route("/", methods=['GET'])
-def test():
-    return "Start challenge!.!.!"
