56b2101fc250f4cb5e3e99fe7dfe54956ba77b16
Simeone-Holanda <simeone.10s10@gmail.com>
2021-08-17 01:09:46 -0300

    [Modified]: Improvements to the dominon layer of cards and tags

--- src/domains/card/card.py
+++ src/domains/card/card.py
@@ -1,15 +1,16 @@
-from typing import Text
-from pydantic import BaseModel
-from typing import List
-from src.domains.tag import Tag
+from typing import List, Union
 from secrets import token_hex
+from pydantic import BaseModel
+from datetime import datetime
+from src.domains.tag import Tag
+
 
 class Card(BaseModel):
     id : str
     text : str
-    data_criacao : str # mudar para datatime no futuro
-    data_modificacao : str # mudar para datatime no futuro
-    tags : List[Tag]
+    data_criacao : datetime 
+    data_modificacao : datetime
+    tags : Union[List[Tag], Tag] = None
 
     @classmethod
     def generate_id(self):
--- src/domains/tag/tag.py
+++ src/domains/tag/tag.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
+from secrets import token_hex
 
 class Tag(BaseModel):
     id : str
     name : str
+
+    @classmethod
+    def generate_id(self):
+        return token_hex(16)
