3dd1e7c2a20db7f4d442695e9ae2a8bca4792d1b
Simeone-Holanda <simeone.10s10@gmail.com>
2021-08-20 09:17:19 -0300

    [Added]: added the cors.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from src.api.card.card_api import CardAPI
 from src.api.tag.tag_api import TagAPI
-
+from flask_cors import CORS
 app = Flask(__name__)
 
 app.secret_key = "my secret key"
+CORS(app)
 
 app.register_blueprint(CardAPI)
 app.register_blueprint(TagAPI)
